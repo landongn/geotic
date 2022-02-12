cc3feb8d11cc3114dd0b0cda19d720a8de9e6066
Landon Noss <landongn@gmail.com>
2022-02-11 19:45:38 -0700

    truthiness for existence will only work if true

--- src/PrefabRegistry.js
+++ src/PrefabRegistry.js
@@ -112,7 +112,7 @@ export class PrefabRegistry {
         const entity = world.createEntity();
 
         entity._qeligible = false;
-        if (properties.serializable) {
+        if (typeof properties.serializable !== 'undefined') {
             entity.serializable = properties.serializable;
         }
 
